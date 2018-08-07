49d66080392897e7c85d9d5677d6bc181c718aec
Klaus Schwarz <schwarz@posteo.de>
2018-08-07 23:39:32 +0200

    moved assets folder for more convinience and added gemfile

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+gem 'nokogiri'
+gem 'diffy'
+gem 'mechanize'
+gem 'mail'
+gem 'fileutils'
--- getGrades.rb
+++ getGrades.rb
@@ -56,13 +56,13 @@ class HisqisMailer
             <meta charset="utf-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link rel="stylesheet" href="../assets/css/main.css" />
-            <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-            <link rel="stylesheet" href="../assets/css/bootstrap-toggle.min.css">
-            <script src="../assets/js/jquery-3.1.1.min.js"></script>
-            <script src="../assets/js/jquery.backstretch.min.js"></script>
-            <script src="../assets/js/bootstrap.min.js"></script>
-            <script src="../assets/js/bootstrap-toggle.min.js"></script>
+            <link rel="stylesheet" href="assets/css/main.css" />
+            <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+            <link rel="stylesheet" href="assets/css/bootstrap-toggle.min.css">
+            <script src="assets/js/jquery-3.1.1.min.js"></script>
+            <script src="assets/js/jquery.backstretch.min.js"></script>
+            <script src="assets/js/bootstrap.min.js"></script>
+            <script src="assets/js/bootstrap-toggle.min.js"></script>
           </head>
           <body>
             <script type="text/javascript" language="javascript">
@@ -176,13 +176,13 @@ def writeIndex
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="../assets/css/main.css" />
-        <link rel="stylesheet" href="../assets/css/bootstrap.min.css" />
-        <link rel="stylesheet" href="../assets/css/bootstrap-toggle.min.css">
-        <script src="../assets/js/jquery-3.1.1.min.js"></script>
-        <script src="../assets/js/jquery.backstretch.min.js"></script>
-        <script src="../assets/js/bootstrap.min.js"></script>
-        <script src="../assets/js/bootstrap-toggle.min.js"></script>
+        <link rel="stylesheet" href="assets/css/main.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
+        <link rel="stylesheet" href="assets/css/bootstrap-toggle.min.css">
+        <script src="assets/js/jquery-3.1.1.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/bootstrap-toggle.min.js"></script>
       </head>
       <body>
         <script type="text/javascript" language="javascript">
